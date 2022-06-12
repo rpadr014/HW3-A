28aa47ae4b883f46c9ab730ed93c8737c1d72d42
NoelAPF <napfnb@gmail.com>
2022-06-12 16:42:23 -0400

    draft 1

--- HW3-A/ellipticForm.Designer.cs
+++ HW3-A/ellipticForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(3860, 1100);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ellipticForm";
             this.Text = "ellipticForm";
